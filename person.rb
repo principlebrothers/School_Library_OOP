6812f06ce619b9dbb1a3120b310d9c7842713025
principlebrothers <principlebrothers@yahoo.com>
2022-09-26 22:26:54 +0000

    Update constructor

--- person.rb
+++ person.rb
@@ -2,8 +2,8 @@ class Person
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(id, age, name = 'Unknown', parent_permission: true)
-    @id = id
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
     @age = age
     @name = name
     @parent_permission = parent_permission
