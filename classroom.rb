dd11aede89e065a83c0dbec128920ed1f7d88101
principlebrothers <principlebrothers@yahoo.com>
2022-09-29 23:39:08 +0000

    Update add_student function

--- classroom.rb
+++ classroom.rb
@@ -9,6 +9,6 @@ class Classroom
 
   def add_student(student)
     @students << student
-    student.classroom = self unless (student.classroom = self)
+    student.classroom = self unless student.classroom.include?(self)
   end
 end
