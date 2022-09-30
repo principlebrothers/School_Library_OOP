fac1d362bffb4e4a9645db1ab76b10e657560ae7
principlebrothers <principlebrothers@yahoo.com>
2022-09-30 18:37:10 +0000

    Create method to create book

--- app.rb
+++ app.rb
@@ -65,5 +65,17 @@ class App
     end
   end
 
+  # create a book
+  def create_book
+    print 'Title: '
+    title = gets.chomp
+
+    print 'Author: '
+    author = gets.chomp
+
+    @books << Book.new(title, author)
+    puts 'Book created successfully'
+  end
+
 
 end
