02ae02505d232db40cd71c3a0309948c05932fcb
principlebrothers <principlebrothers@yahoo.com>
2022-10-01 01:42:57 +0000

    Update constructor

--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,7 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(specialization)
+  def initialize(specialization, age, name = 'Unknown')
     super(age, name)
     @specialization = specialization
   end
